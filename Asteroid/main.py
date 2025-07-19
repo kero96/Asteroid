ca02fc81f2306a1871f06e9cd1f54d9c8aa29782
kero96 <smm21196@gmail.com>
2025-07-19 13:12:57 +0200

    main.py added

--- Asteroid/main.py
+++ Asteroid/main.py
@@ -1,16 +1,25 @@
 import pygame as pg
 import constants as c
+
 def main():
     pg.init()
     screen = pg.display.set_mode((c.SCREEN_WIDTH, c.SCREEN_HEIGHT))
+
+    pg.time.Clock().tick(60)  # Set the frame rate to 60 FPS
+    dt = 0.0  # Initialize delta time
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 pg.quit()
                 return
+
         fill_color = (0, 0, 0)  # Black background
         screen.fill(fill_color)
         pg.display.flip()
+
+        dt = pg.time.Clock().tick(60) / 1000.0
+
     print("Starting Asteroids!")
     print(f"Screen width: {c.SCREEN_WIDTH}")
     print(f"Screen height: {c.SCREEN_HEIGHT}")
